0794fde6f601d7d18ec1caac9784d5e417dd64ab
Alfonso Rodriguez <Alfonsorg95@gmail.com>
2022-08-06 18:05:51 -0400

    scrapper working

---
+++ scrapper.py
@@ -0,0 +1,79 @@
+from turtle import title
+from urllib import response
+import requests
+import lxml.html as html
+import os
+import datetime
+
+HOME_URL = 'https://www.larepublica.co'
+XPATH_LINK_TO_ARTICLE = '//div/text-fill/a[@class and contains(@href,"larepublica")]/@href'
+XPATH_TITLE = '//div[@class = "mb-auto"]/h2/span/text()'
+XPATH_SUMMARY = '//div[@class = "lead"]/p/text()'
+XPATH_BODY = '//div[@class = "html-content"]/p//text()'
+
+
+def parse_news(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            news = response.content.decode('utf-8')
+            parsed = html.fromstring(news)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"','').strip()
+                
+                if parsed.xpath(XPATH_SUMMARY)[0]:
+                    summary = parsed.xpath(XPATH_SUMMARY)[0]
+                else:
+                    summary = None
+                
+                body = parsed.xpath(XPATH_BODY)
+
+            except IndexError:
+                return
+
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                if summary != None:
+                    f.write(summary)
+                    f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+
+    except ValueError:
+        print(ValueError)
+
+
+def parse_home():
+    try:
+        response = requests.get(HOME_URL)
+
+        if response.status_code == 200:
+            home = response.content.decode('utf-8')
+            parsed = html.fromstring(home)
+            links_to_news = parsed.xpath(XPATH_LINK_TO_ARTICLE)
+
+            today = datetime.date.today().strftime('%d-%m-%Y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+
+            for link in links_to_news:
+                parse_news(link, today)
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+
+    except ValueError:
+        print(ValueError)
+
+def main():
+    parse_home()
+
+if __name__ == '__main__':
+    main()
